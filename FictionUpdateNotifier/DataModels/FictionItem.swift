b0ccfcdba965f045e5398641e85a6d948a4135df
zhitao zhou <davidzhou0621@gmail.com>
2018-10-03 20:47:25 -0700

    Initial UITableViewController Setup and Navigation View Controller

---
+++ FictionUpdateNotifier/DataModels/FictionItem.swift
@@ -0,0 +1,13 @@
+//
+//  FictionItem.swift
+//  FictionUpdateNotifier
+//
+//  Created by david zhou on 10/3/18.
+//  Copyright © 2018 David Zhou. All rights reserved.
+//
+
+import Foundation
+
+class FictionItem {
+    
+}
