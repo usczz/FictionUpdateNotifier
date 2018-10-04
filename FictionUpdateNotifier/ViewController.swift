d492a7c03821c22b69b2bdaee8f29c2a20046502
zhitao zhou <davidzhou0621@gmail.com>
2018-10-03 20:07:10 -0700

    Initial Commit

---
+++ FictionUpdateNotifier/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FictionUpdateNotifier
+//
+//  Created by david zhou on 10/3/18.
+//  Copyright © 2018 David Zhou. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
